2c223866c9dea6a3847d6d54219b0a93358a8434
SleePerwtm <wtm2785419816@outlook.com>
2025-05-28 12:45:10 +0800

    feat: 添加material for mkdocs配置，并创建github工作流

---
+++ docs/javascripts/config.js
@@ -0,0 +1,13 @@
+/* MathJax 配置，来自 Material for MkDocs 文档 */
+window.MathJax = {
+    tex: {
+        inlineMath: [["\\(", "\\)"]],
+        displayMath: [["\\[", "\\]"]],
+        processEscapes: true,
+        processEnvironments: true
+    },
+    options: {
+        ignoreHtmlClass: ".*|",
+        processHtmlClass: "arithmatex"
+    }
+};
